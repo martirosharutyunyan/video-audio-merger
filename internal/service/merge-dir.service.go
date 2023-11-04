66082a5b5727c5bbfbf83bfdf5d652cc27b74f5d
martirosharutyunyan <martiros.harutunyan@gmail.com>
2023-11-04 13:59:46 +0400

    refact

--- internal/service/merge-dir.service.go
+++ internal/service/merge-dir.service.go
@@ -1,17 +1,66 @@
 package service
 
+import (
+	"io/fs"
+	"os"
+	"path/filepath"
+	"runtime"
+	"strings"
+)
+
 type IMergeVideoAudioDirService interface {
-    Merge(source, output string) error
+	Merge(source, output string) error
 }
 
-type MergeVideoAudioDirService struct {}
+type MergeVideoAudioDirService struct {
+	mergeVideoAudioFileService IMergeVideoAudioFileService
+}
 
-func (m MergeVideoAudioDirService) Merge(source, output string) error {
+var _ IMergeVideoAudioDirService = MergeVideoAudioDirService{}
 
-	return nil
+func (s MergeVideoAudioDirService) Merge(source, output string) error {
+
+	err := filepath.WalkDir(source, func(path string, _ fs.DirEntry, err error) error {
+		if source == path {
+			return nil
+		}
+		
+		if strings.Contains(source, "audio") {
+			return nil
+		}
+
+		info, err := os.Stat(path)
+		if err != nil {
+			return err
+		}
+
+		if runtime.GOOS == "windows" {
+			path = strings.ReplaceAll(path, "\\", "/")
+		}
+
+		fileRelativePath := strings.Split(path, source)[1]
+
+		var outputBuilder strings.Builder
+		outputBuilder.WriteString(output)
+		outputBuilder.WriteString(fileRelativePath)
+
+		audioPath := strings.Replace(path, ".mp4", "_audio.mp4", -1)
+		
+		if info.IsDir() {
+			err = os.Mkdir(outputBuilder.String(), 0777)
+			if err != nil {
+				return err
+			}
+		} else {
+			return s.mergeVideoAudioFileService.Merge(path, audioPath, outputBuilder.String())
+		}
+
+		return err
+	})
+
+	return err
 }
 
 func NewMergeVideoAudioDirService() *MergeVideoAudioDirService {
 	return &MergeVideoAudioDirService{}
 }
-
